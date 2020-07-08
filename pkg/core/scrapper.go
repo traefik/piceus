e79b8c5aee47e5c9918624749a9d84375ac9262b
Fernandez Ludovic <ludovic@containo.us>
2020-07-08 15:20:04 +0200

    fix: import and module name relation.

--- pkg/core/scrapper.go
+++ pkg/core/scrapper.go
@@ -224,7 +224,7 @@ func (s *Scrapper) process(ctx context.Context, repository *github.Repository) (
 
 	// Checks consistency
 
-	if !strings.HasPrefix(manifest.Import, moduleName) {
+	if !strings.HasPrefix(manifest.Import, strings.ReplaceAll(moduleName, "-", "_")) {
 		return nil, fmt.Errorf("the import %q must be related to the module name %q", manifest.Import, moduleName)
 	}
 
