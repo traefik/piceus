71c3863f132dbd3eee132103368385e958574790
Trois-Six <trois.six@free.fr>
2020-10-26 16:18:10 +0100

    feat: clean-up path

--- pkg/core/scrapper.go
+++ pkg/core/scrapper.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"reflect"
@@ -378,6 +379,20 @@ func (s *Scrapper) loadManifest(ctx context.Context, repository *github.Reposito
 
 	if m.Summary == "" {
 		return Manifest{}, errors.New("missing Summary")
+	}
+
+	pict, err := url.Parse(m.IconURL)
+	if err != nil {
+		m.IconURL = ""
+	} else {
+		m.IconURL = path.Clean(pict.EscapedPath())
+	}
+
+	pict, err = url.Parse(m.BannerURL)
+	if err != nil {
+		m.BannerURL = ""
+	} else {
+		m.BannerURL = path.Clean(pict.EscapedPath())
 	}
 
 	if m.TestData == nil {
