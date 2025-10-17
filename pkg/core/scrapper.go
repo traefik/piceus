16021c9f24361f7bcb6e152ef34d250d28b98509
Rémi BUISSON <remi.buisson@traefik.io>
2025-10-17 10:30:04 +0200

    fix: blacklist iobear/queryparameter-to-bearer

--- pkg/core/scrapper.go
+++ pkg/core/scrapper.go
@@ -105,6 +105,7 @@ func NewScrapper(gh *github.Client, gp *goproxy.Client, pgClient pluginClient, d
 			"deas/teectl":                               {}, // Not a plugin
 			"GDGVIT/securum-exire":                      {}, // Not a plugin
 			"morzan1001/forward_auth_grpc_plugin":       {}, // piceus panic (excluded during fix)
+			"iobear/queryparameter-to-bearer":           {}, // Doesn't allow issues
 		},
 		skipNewCall: map[string]struct{}{
 			"github.com/negasus/traefik-plugin-ip2location": {},
